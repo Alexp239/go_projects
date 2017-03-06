754070c0a77849b7e9de96851b8afa94d595a599
Portnoy Alexander <alexp239.14@gmail.com>
2017-03-06 10:01:21 +0300

    funcs_0 Sqrt done.

--- 2/99_homework/funcs/0/main.go
+++ 2/99_homework/funcs/0/main.go
@@ -4,9 +4,23 @@ import (
 	"fmt"
 )
 
-// TODO: Реализовать вычисление Квадратного корня
+func myAbs(x float64) float64 {
+	if x >= 0 {
+		return x
+	}
+	return -x
+}
+
+// Sqrt - вычисление Квадратного корня
 func Sqrt(x float64) float64 {
-	return x
+	var xPrev float64
+	var xCur = float64(1.0)
+	eps := 0.0001
+	for myAbs(xCur-xPrev) > eps {
+		xPrev = xCur
+		xCur = (0.5 * (xCur + x/xCur))
+	}
+	return xCur
 }
 
 func main() {
