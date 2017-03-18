d96d8dddf7aff1d7656d83f147cf7aca2f76f2d3
Portnoy Alexander <alexp239.14@gmail.com>
2017-03-19 01:42:00 +0300

    1_pipeline done.

--- 3/99_homework/1_pipeline/pipeline.go
+++ 3/99_homework/1_pipeline/pipeline.go
@@ -1,7 +1,25 @@
 package pipeline
 
+import "sync"
+
 type job func(in, out chan interface{})
 
 func Pipe(funcs ...job) {
+	var wg sync.WaitGroup
+	wg.Add(len(funcs))
+	var channels []chan interface{}
+	for i := 0; i < len(funcs)+2; i++ {
+		channels = append(channels, make(chan interface{}))
+	}
+	for i, fun := range funcs {
+		go func(i int, f job) {
+			curIn := channels[i]
+			curOut := channels[i+1]
+			defer wg.Done()
+			defer close(curOut)
+			f(curIn, curOut)
+		}(i, fun)
+	}
+	wg.Wait()
 	return
 }
