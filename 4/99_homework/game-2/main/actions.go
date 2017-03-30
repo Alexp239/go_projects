0d5164ad7310175bb86a153bad3c595b82e0c65f
Portnoy Alexander <alexp239.14@gmail.com>
2017-03-30 11:49:26 +0300

    Game-bot small changes and cod adding.

---
+++ 4/99_homework/game-2/main/actions.go
@@ -0,0 +1,213 @@
+package main
+
+// Look - осмотреться
+func (player *Player) Look(ar ...string) string {
+	r := player.position
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	res := r.lookDiscr
+
+	showRes, fl := player.ShowLockers()
+	res += showRes
+
+	if len(r.furniture) > 0 {
+		for _, furn := range r.furniture {
+			if len(furn.items) > 0 {
+				if fl {
+					res += ", "
+				}
+				fl = true
+				res += furn.description
+				i := false
+				for _, item := range furn.items {
+					if i {
+						res += ", "
+					}
+					i = true
+					res += item.name
+				}
+			}
+		}
+	}
+	if fl == false {
+		res += player.position.freeDiscr
+	}
+
+	if player.position.name == "кухня" {
+		tasksRes := player.checkTasks()
+		if tasksRes != "" {
+			res += ", надо " + tasksRes
+		}
+	}
+
+	res += "."
+	res += player.AddNeighbRooms()
+	res += player.AddPlayers()
+	return res
+}
+
+// GoRoom - идти
+func (player *Player) GoRoom(par ...string) string {
+	if len(par) == 0 {
+		return "Команда идти [комната]"
+	}
+	for _, path := range player.position.paths {
+		if path.room.name == par[0] {
+			if path.locker != nil && path.locker.locked == true {
+				return path.locker.name + path.locker.closeDiscr
+			}
+			r := player.position
+			r.mu.Lock()
+			defer r.mu.Unlock()
+			path.room.mu.Lock()
+			defer path.room.mu.Unlock()
+			delete(r.players, player.id)
+			player.position = path.room
+			player.position.players[player.id] = player
+
+			res := path.room.goDiscr
+			res += player.AddNeighbRooms()
+			return res
+		}
+	}
+	return "нет пути в " + par[0]
+}
+
+// Take - взять
+func (player *Player) Take(par ...string) string {
+	if len(par) == 0 {
+		return "Команда взять [предмет]"
+	}
+	r := player.position
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	if player.inventory == nil {
+		return "некуда класть"
+	}
+	takeItem := par[0]
+	for _, furn := range r.furniture {
+		for i, item := range furn.items {
+			if item.name == takeItem {
+				furn.TakeItem(i, player)
+				return "предмет добавлен в инвентарь: " + takeItem
+			}
+		}
+	}
+	return "нет такого"
+}
+
+// Dress - одеть
+func (player *Player) Dress(par ...string) string {
+	if len(par) == 0 {
+		return "Команда одеть [предмет]"
+	}
+	r := player.position
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	takeItem := par[0]
+	if takeItem == "рюкзак" {
+		for _, furn := range r.furniture {
+			for i, item := range furn.items {
+				if item.name == takeItem {
+					furn.DressBag(i, player)
+					return "вы одели: " + takeItem
+				}
+			}
+		}
+	} else {
+		return "неизвестная команда"
+	}
+	return "нет такого"
+}
+
+// Apply - применить
+func (player *Player) Apply(par ...string) string {
+	if len(par) < 2 {
+		return "Команда применить [предмет] [замок]"
+	}
+	r := player.position
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	applyItem := par[0]
+	applyFurn := par[1]
+
+	var item *Item
+	var ok bool
+
+	if item, ok = player.inventory[applyItem]; !ok {
+		return "нет предмета в инвентаре - " + applyItem
+	}
+
+	for _, path := range player.position.paths {
+		if path.locker != nil && path.locker.name == applyFurn {
+			if path.locker.unlockItem == item && path.locker.locked == true {
+				path.locker.Unlock()
+				return path.locker.name + path.locker.openDiscr
+			}
+		}
+	}
+
+	return "не к чему применить"
+}
+
+// SayPlayer - сказать игроку
+func (player *Player) SayPlayer(par ...string) {
+	if len(par) < 1 {
+		player.HandleOutput("сказать_игроку [игрок] [сообщение]")
+		return
+	}
+	r := player.position
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	ok := false
+	var reciever *Player
+	for _, pl := range r.players {
+		if pl.name == par[0] {
+			if ok {
+				player.HandleOutput("тут несколько игроков с такими именами")
+				return
+			}
+			reciever = pl
+			ok = true
+		}
+	}
+
+	if !ok {
+		player.HandleOutput("тут нет такого игрока")
+		return
+	}
+	if len(par) == 1 {
+		reciever.HandleOutput(player.name + " выразительно молчит, смотря на вас")
+		return
+	}
+	res := player.name + " говорит вам:"
+	for _, word := range par[1:] {
+		res += " " + word
+	}
+	reciever.HandleOutput(res)
+	return
+}
+
+// Say - сказать всем
+func (player *Player) Say(par ...string) {
+	r := player.position
+
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	res := ""
+	if len(par) == 0 {
+		res += player.name + " выразительно молчит"
+	} else {
+		res += player.name + " говорит:"
+		for _, word := range par {
+			res += " " + word
+		}
+	}
+
+	for _, pl := range r.players {
+		pl.HandleOutput(res)
+	}
+	return
+}
---
+++ 4/99_homework/game-2/main/main.go
@@ -0,0 +1,226 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"gopkg.in/telegram-bot-api.v4"
+)
+
+var players []*Player
+var startPosition *Room
+var actions []string
+var admins map[int]bool
+
+func NewPlayer(playerID int, name string, chatID int64) *Player {
+	player := Player{
+		id:       playerID,
+		name:     name,
+		messages: make(chan string),
+		chatID:   chatID,
+	}
+	players = append(players, &player)
+	return &player
+}
+
+func addPlayer(player *Player) {
+	player.initTasks()
+	player.position = startPosition
+	startPosition.players[player.id] = player
+}
+
+func initRooms() {
+	roomTable := Furniture{
+		name:        "стол",
+		description: "на столе: ",
+	}
+
+	keys := Item{
+		name:         "ключи",
+		defaultPlace: &roomTable,
+	}
+	roomTable.AddItem(&keys)
+	conspects := Item{
+		name:         "конспекты",
+		defaultPlace: &roomTable,
+	}
+	roomTable.AddItem(&conspects)
+
+	roomChair := Furniture{
+		name:        "стул",
+		description: "на стуле - ",
+	}
+	roomChair.AddItem(&Item{
+		name:         "рюкзак",
+		defaultPlace: &roomChair,
+	})
+
+	kitchen := Room{
+		name:      "кухня",
+		lookDiscr: "ты находишься на кухне, на столе чай",
+		goDiscr:   "кухня, ничего интересного.",
+		players:   PlayersMap{},
+	}
+	corridor := Room{
+		name:    "коридор",
+		goDiscr: "ничего интересного.",
+		players: PlayersMap{},
+	}
+	room := Room{
+		name:      "комната",
+		goDiscr:   "ты в своей комнате.",
+		freeDiscr: "пустая комната",
+		players:   PlayersMap{},
+	}
+	street := Room{
+		name:    "улица",
+		goDiscr: "на улице весна.",
+		players: PlayersMap{},
+	}
+
+	kitchen.AddPath(&corridor)
+	corridor.AddPath(&room)
+
+	door := Locker{
+		Furniture: Furniture{
+			name:        "дверь",
+			description: "",
+		},
+		locked:     true,
+		unlockItem: &keys,
+		openDiscr:  " открыта",
+		closeDiscr: " закрыта",
+	}
+
+	corridor.AddPathLocked(&street, &door)
+
+	room.AddFurniture(&roomTable)
+	room.AddFurniture(&roomChair)
+
+	startPosition = &kitchen
+}
+
+func initGame() {
+	initRooms()
+	actions = []string{"/start", "/help", "осмотреться", "идти [комната]",
+		"взять [предмет]", "одеть [предмет]", "применить [предмет] [замок]",
+		"сказать_игроку [игрок] [сообщение]", "сказать [сообщение]"}
+	players = []*Player{}
+	admins = map[int]bool{47394442: true}
+}
+
+func findPlayer(name string) (player *Player, ok bool) {
+	for _, player = range players {
+		if player.name == name {
+			ok = true
+			return
+		}
+	}
+	return
+}
+
+func getCommandsInfo(player *Player) {
+	s := "Возможные команды:\n"
+	for _, act := range actions {
+		s += act + "\n"
+	}
+	player.HandleOutput(s)
+}
+
+func getHiCommand(player *Player) {
+	player.HandleOutput("Вы в игре!")
+}
+
+var buttons = []tgbotapi.KeyboardButton{
+	tgbotapi.KeyboardButton{Text: "Сброс игры"},
+}
+
+// При старте приложения, оно скажет телеграму ходить с обновлениями по этому URL
+const WebhookURL = "https://alexp-go-game.herokuapp.com/bot"
+
+func main() {
+	// Heroku прокидывает порт для приложения в переменную окружения PORT
+	port := os.Getenv("PORT")
+	bot, err := tgbotapi.NewBotAPI("367628029:AAEgmLPXWDhXvXH6WH9jxwBRU5cbhantK5k")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	// Устанавливаем вебхук
+	_, err = bot.SetWebhook(tgbotapi.NewWebhook(WebhookURL))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	initGame()
+
+	updates := bot.ListenForWebhook("/bot")
+	go http.ListenAndServe(":"+port, nil)
+
+	go func() {
+		for {
+			time.Sleep(time.Minute)
+			for i, pl := range players {
+				if time.Now().Sub(pl.lastMessageTime) > time.Minute*15 {
+					pl.DeletePlayer()
+					players = append(players[:i], players[i+1:]...)
+				}
+			}
+		}
+	}()
+
+	// получаем все обновления из канала updates
+	for update := range updates {
+		log.Println("received text: ", update.Message.Text)
+		name := update.Message.From.UserName
+		id := update.Message.From.ID
+		if name == "" {
+			name = update.Message.From.FirstName + update.Message.From.LastName
+		}
+		player, ok := findPlayer(name)
+		if !ok {
+			player = NewPlayer(id, name, update.Message.Chat.ID)
+
+			go func(player *Player, id int) {
+				var message tgbotapi.MessageConfig
+				output := player.GetOutput()
+				for msg := range output {
+					message = tgbotapi.NewMessage(player.chatID, msg)
+					if admins[id] {
+						message.ReplyMarkup = tgbotapi.NewReplyKeyboard(buttons)
+					}
+					bot.Send(message)
+				}
+
+			}(player, id)
+
+			addPlayer(player)
+		}
+
+		player.lastMessageTime = time.Now()
+
+		switch update.Message.Text {
+		case "/start":
+			getHiCommand(player)
+			fallthrough
+		case "/help":
+			getCommandsInfo(player)
+		case "Сброс игры":
+			for _, pl := range players {
+				pl.DeletePlayer()
+			}
+			initGame()
+		default:
+			player.HandleInput(update.Message.Text)
+		}
+
+		// В ответном сообщении просим показать клавиатуру
+
+	}
+}
